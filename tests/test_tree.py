0a9b311c82310cfc4237ebe94654228f030e325c
vesran <tran.yves@gmail.com>
2020-05-03 19:30:16 +0200

    tests for trees

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,10 +1,11 @@
 from tools.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
+import random as rand
 
 
 def test_gini_and_info_gain():
-    filename = 'data/tennis.dat'
+    filename = 'resources/tennis.dat'
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -30,7 +31,7 @@ def test_gini_and_info_gain():
 
 
 def test_decision_tree():
-    df = pd.read_csv('./data/titanic.csv', sep=',').drop(['Fare', 'Name', 'Age'], axis=1)
+    df = pd.read_csv('./resources/titanic.csv', sep=',').drop(['Fare', 'Name', 'Age'], axis=1)
     y = df['Survived'].values
     X = df.drop('Survived', axis=1).values
     dtree = DecisionTreeClassifier(max_depth=10)
@@ -39,3 +40,12 @@ def test_decision_tree():
     assert dtree._single_predict(X[0]) == y[0]
     assert dtree._single_predict(X[1]) == y[1]
     assert dtree._single_predict(X[20]) == y[20]
+
+
+def test_decision_tree_max_depth():
+    n = 1000
+    X = np.array([[rand.choice([1, 2, 3]) for _ in range(20)] for _ in range(n)])
+    y = np.array([rand.choice(['a', 'b', 'c']) for _ in range(n)])
+    dtree = DecisionTreeClassifier(max_depth=3)
+    dtree.fit(X, y)
+    assert dtree.children[0].children[0].children[0].children is None
