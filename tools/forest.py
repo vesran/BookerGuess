f349f77ede594843fa3741dce3d9ff9a76045d28
vesran <tran.yves@gmail.com>
2020-05-04 16:41:02 +0200

    added sampfactor parameter

--- tools/forest.py
+++ tools/forest.py
@@ -6,38 +6,35 @@ import random as rand
 
 class RandomForestClassifier:
 
-    def __init__(self, n_estimators=1000, max_depth=1000, features_names=None):
+    def __init__(self, n_estimators=1000, max_depth=1000, features_names=None, sampfactor=1):
         self.n_estimators=n_estimators
         self.max_depth = max_depth
         self.n_features = None
         self.trees = [DecisionTreeClassifier(max_depth=self.max_depth, random_features=True, label_names=features_names)
                       for _ in range(self.n_estimators)]
         self.X, self.y = None, None
+        self.sampfactor = sampfactor
 
     def fit(self, X, y):
+        sampsize = int(X.shape[0] * self.sampfactor)
+        assert sampsize > 0
         for dtree in self.trees:
             # Bootstrap
             X_train = []
             y_train = []
-            for j in range(X.shape[0]):
-                i = rand.randint(0, X.shape[0]-1)*0 + j
+            for j in range(sampsize):
+                i = rand.randint(0, X.shape[0]-1)
                 X_train.append(X[i])
                 y_train.append(y[i])
             X_train, y_train = np.array(X_train), np.array(y_train)
             assert X_train.dtype == X.dtype
-            assert X_train.shape[0] == X.shape[0]
 
             # Training the tree
             dtree.fit(X_train, y_train)
 
-    def _single_predict(self, x):
-        # votes = []
-        # for i, dtree in enumerate(self.trees):
-        #     print(f"{i}e tree")
-        #     votes.append(dtree._single_predict(x))
-
+    def _single_predict(self, x, debug=False):
         votes = [dtree._single_predict(x) for dtree in self.trees]
-        print(votes)
+        print(f"Votes : {votes}") if debug else 0
         return Counter(votes).most_common(1)[0][0]
 
     def predict(self, X):
@@ -57,7 +54,7 @@ if __name__ == '__main__':
     n = 1000
     X = np.array([[rand.choice([1, 2, 3]) for _ in range(20)] for _ in range(n)])
     y = np.array([rand.choice(['a', 'b', 'c']) for _ in range(n)])
-    rf = RandomForestClassifier(max_depth=10, n_estimators=10)
+    rf = RandomForestClassifier(max_depth=10, n_estimators=30, sampfactor=0.6)
     rf.fit(X[:900], y[:900])
 
-    rf._single_predict(X[903])
+    rf._single_predict(X[6], debug=True)
