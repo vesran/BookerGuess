0023bf278ba3db4cf5f05597561d5cbdf58301e6
vesran <tran.yves@gmail.com>
2020-05-19 17:04:44 +0200

    f1 score in prod

--- tools/metrics.py
+++ tools/metrics.py
@@ -24,7 +24,7 @@ def confusion_matrix(classifier, X, y, normalize=True, decimal=2, plot=False):
         ax.set_xticklabels([0] + labels.tolist())
         ax.set_yticklabels([0] + labels.tolist())
         for (i, j), z in np.ndenumerate(counts):
-            color = 'black' if counts[i][j] <= 0.5 else 'white'
+            color = 'black' if counts[i][j] <= 0.5 or not normalize else 'white'
             ax.text(j, i, f'{{:0.{decimal}f}}'.format(z), ha='center', va='center', c=color)
     return counts
 
@@ -39,16 +39,20 @@ def precision(bi_confmat):
     return bi_confmat[0][0] / (bi_confmat[0][0] + bi_confmat[1][0])
 
 
-def f1score(model, X, y):
-    bi_confmat = confusion_matrix(model, X, y, plot=False, decimal=6)
-    prec = precision(bi_confmat)
-    rec = recall(bi_confmat)
+def f1_score(y_pred, y_true):
+    labels = np.unique(y)
+    counts = np.zeros((labels.shape[0], labels.shape[0]))
+    for prediction, truth in zip(y_pred, y_true):
+        i_truth = np.where(labels == truth)[0][0]
+        i_prediction = np.where(labels == prediction)[0][0]
+        counts[i_truth][i_prediction] += 1
+    prec = precision(counts)
+    rec = recall(counts)
     return 2 * rec * prec / (rec + prec)
 
 
-def score(model, X, y):
-    y_pred = model.predict(X)
-    return np.sum((y_pred == y).astype(int)) / y.shape[0]
+def score(y_pred, y_true):
+    return np.sum((y_pred == y_true).astype(int)) / y_true.shape[0]
 
 
 def cross_validation_score(model, X, y, k=3, scorer=score):
@@ -70,7 +74,8 @@ def cross_validation_score(model, X, y, k=3, scorer=score):
 
         # Train and score
         model.fit(train_X, train_y)  # Suppose fit makes the model forget previously learnt data
-        scores.append(scorer(model, partition_X[i], partition_y[i]))
+        y_pred = model.predict(partition_X[i])
+        scores.append(scorer(y_pred, partition_y[i]))
     return np.array(scores)
 
 
@@ -85,4 +90,4 @@ if __name__ == '__main__':
     model = DecisionTreeClassifier()
     cross_val_score(model, X, y).mean()
 
-    cross_validation_score(model, X, y, k=10).mean()
+    cross_validation_score(model, X, y, k=10, scorer=f1_score).mean()
