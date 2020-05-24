020122562145ce619749f80c634fa388ef4e87c1
vesran <tran.yves@gmail.com>
2020-05-24 11:12:08 +0200

    pca in prod

---
+++ tools/pca.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def PCA(data, k=2):
+    data = data - data.mean(axis=0)
+    data = data / data.std(axis=0)
+    R = np.cov(data.T, rowvar=True)
+    evals, evecs = np.linalg.eig(R)
+
+    # Sort eigenvalue in decreasing order and select the k corresponding
+    # largest eigenvectors
+    idx = np.argsort(evals)[::-1]
+    evecs = evecs[:, idx]
+    evecs = evecs[:, :k]
+    return np.dot(data, evecs)
